160990a8cb40f03c988c23a8234788ae49027940
CHARATSIDIS Vasileios <vcharatsidis@europe.corp.altran.com>
2018-04-25 11:09:20 +0200

    Condition to draw horizontal lines properly implemented

--- Main.cpp
+++ Main.cpp
@@ -8,19 +8,33 @@ using std::vector;
 using std::cout;
 
 void print(vector<vector<int>> board) {
-	int size = board.size();
+	const int horizontalLines = 2;
+	int boardHeight = board.size() + horizontalLines;
+	int boardWidth = board.size();
 	std::ofstream myfile;
 	myfile.open("C:\\Users\\vcharatsidis\\Desktop\\sudoku.txt");
+	int horizontalLineCounter = 0 ;
 
-	for (int i = 0; i < size; i++) {
-		for (int j = 0; j < size; j++) {
-			if (j % 3 == 0 && j>0 ) {
-				myfile << "| "+std::to_string(board[i][j]) + " ";
-			}
-			else {
-				myfile << std::to_string(board[i][j]) + " ";
+	for (int row = 0; row < boardHeight; row++) {
+		bool drawHorizontalLine = ((row + 1) % 4 == 0);
+
+		if (drawHorizontalLine) {
+			myfile << "----------------------";
+			horizontalLineCounter++;
+		}
+		else {
+			for (int column = 0; column < boardWidth; column++) {
+				bool drawVerticalLine = (column % 3 == 0 && column > 0);
+
+				if (drawVerticalLine) {
+					myfile << "| " + std::to_string(board[row - horizontalLineCounter][column]) + " ";
+				}
+				else {
+					myfile << std::to_string(board[row - horizontalLineCounter][column]) + " ";
+				}
 			}
 		}
+		
 		myfile << "\n";
 	}
 	myfile.close();
