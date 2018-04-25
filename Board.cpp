8bfaad6c649e071d57b5ca617014c572a56f02ec
CHARATSIDIS Vasileios <vcharatsidis@europe.corp.altran.com>
2018-04-25 15:28:56 +0200

    split to .h main.cpp and Board.cpp, compiling but not produce expected result

--- Board.cpp
+++ Board.cpp
@@ -1,64 +1,55 @@
-#include<array>;
-#include<vector>;
-#include<iostream>;
-#include<string>;
-#include<fstream>;
-#include<string>;
+#include "Board.h";
 
-using std::vector;
-using std::cout;
+string Board::filePath = "C:\\Users\\vcharatsidis\\Desktop\\sudoku.txt";
+string Board::horizontal_line_serparator = "----------------------";
+string Board::vertical_line_separator = "| ";
+vector<vector<int>> Board::board(9, vector<int>(9, 0));
 
-class Board {
+Board::Board(vector<vector<int>> board) {
 
-public:
-	
-	static vector<vector<int>> board;
-	static int size;
-	std::string const filePath = "C:\\Users\\vcharatsidis\\Desktop\\sudoku.txt";
-	std::string const horizontal_line_serparator = "----------------------";
-	std::string const vertical_line_separator = "| ";
+	int boardSize = board.size();
 
-	Board(vector<vector<int>> board) {
-		size = board.size();
-
-		for (int i = 0; i < size; i++) {
-			for (int j = 0; j < size; j++) {
-				Board::board[i][j] = board[i][j];
-			}
+	for (int i = 0; i < boardSize; i++) {
+		for (int j = 0; j < boardSize; j++) {
+			Board::board[i][j] = board[i][j];
 		}
 	}
+}
 
-	void print(vector<vector<int>> board) {
-		const int horizontalLines = 2;
-		int boardHeight = board.size() + horizontalLines;
-		int boardWidth = board.size();
-		std::ofstream myfile;
-		myfile.open(filePath);
-		int horizontalLineCounter = 0;
+void Board::drawBoard() {
+	const int horizontalLines = 2;
+	int boardSize = board.size();
 
-		for (int row = 0; row < boardHeight; row++) {
-			bool drawHorizontalLine = ((row + 1) % 4 == 0);
+	int drawHeight = boardSize + horizontalLines;
+	int drawWidth = boardSize;
 
-			if (drawHorizontalLine) {
-				myfile << horizontal_line_serparator;
-				horizontalLineCounter++;
-			}
-			else {
-				for (int column = 0; column < boardWidth; column++) {
-					bool drawVerticalLine = (column % 3 == 0 && column > 0);
+	std::ofstream myfile;
+	myfile.open(filePath);
 
-					if (drawVerticalLine) {
-						myfile << vertical_line_separator;
-					}
-				
-					int value = board[row - horizontalLineCounter][column];
-					myfile << std::to_string(value) + " ";
+	int horizontalLineCounter = 0;
+
+	for (int row = 0; row < drawHeight; row++) {
+		bool drawHorizontalLine = ((row + 1) % 4 == 0);
+
+		if (drawHorizontalLine) {
+			myfile << horizontal_line_serparator;
+			horizontalLineCounter++;
+		}
+		else {
+			for (int column = 0; column < drawWidth; column++) {
+				bool drawVerticalLine = (column % 3 == 0 && column > 0);
+
+				if (drawVerticalLine) {
+					myfile << vertical_line_separator;
 				}
+				
+				int value = board[row - horizontalLineCounter][column];
+				myfile << std::to_string(value) + " ";
 			}
-
-			myfile << "\n";
 		}
-		myfile.close();
+		myfile << "\n";
 	}
+	myfile.close();
+}
 
-};
+
--- Main.cpp
+++ Main.cpp
@@ -1,47 +1,10 @@
-#include<array>;
-#include<vector>;
-#include<iostream>;
-#include<string>;
-#include<fstream>;
-
-using std::vector;
-using std::cout;
-
-void print(vector<vector<int>> board) {
-	const int horizontalLines = 2;
-	int boardHeight = board.size() + horizontalLines;
-	int boardWidth = board.size();
-	std::ofstream myfile;
-	myfile.open("C:\\Users\\vcharatsidis\\Desktop\\sudoku.txt");
-	int horizontalLineCounter = 0 ;
-
-	for (int row = 0; row < boardHeight; row++) {
-		bool drawHorizontalLine = ((row + 1) % 4 == 0);
-
-		if (drawHorizontalLine) {
-			myfile << "----------------------";
-			horizontalLineCounter++;
-		}
-		else {
-			for (int column = 0; column < boardWidth; column++) {
-				bool drawVerticalLine = (column % 3 == 0 && column > 0);
-
-				if (drawVerticalLine) {
-					myfile << "| " + std::to_string(board[row - horizontalLineCounter][column]) + " ";
-				}
-				else {
-					myfile << std::to_string(board[row - horizontalLineCounter][column]) + " ";
-				}
-			}
-		}
-		
-		myfile << "\n";
-	}
-	myfile.close();
-}
+#include "Board.h";
+//#include<vector>;
+//using std::vector;
 
 int main() {
 	vector<vector<int>> hardestSudoku(9, vector<int>(9,0));
-	print(hardestSudoku);
+	Board* sudokuBoard = new Board(hardestSudoku);
+	sudokuBoard->drawBoard;
 
 };
